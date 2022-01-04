109acca3870229d02194909e28bdcfec272ba8c4
Johannes Kares <jk@talentir.com>
2022-01-04 18:54:56 +0100

    first talentir tests

---
+++ contracts/Talentir.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/// @custom:security-contact jk@talentir.com
+contract Talentir is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC721("Talentir", "TAL") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = uint256(sha256(abi.encodePacked((uri))));
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,36 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Talentir", function () {
+  it("Test, async function ()", async function() {
+    const Talentir = await ethers.getContractFactory("Talentir");
+    const talentir = await Talentir.deploy();
+    await talentir.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const signers = await ethers.getSigners();
+    const adminAddress = await signers[0].getAddress();
+    const minterAddress = await signers[1].getAddress();
+    const user1Address = await signers[2].getAddress();
+    
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // const mintTx = await talentir.safeMint(user1Address, "abcd");
+    const uri = "sdlkfje";
+    const tokenID = ethers.utils.sha256(ethers.utils.toUtf8Bytes(uri));
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await expect(talentir.safeMint(user1Address, uri))
+      .to.emit(talentir, "Transfer").withArgs(ethers.constants.AddressZero, user1Address, tokenID)
+      .to.not.emit(talentir, "Approval");
+  
+    expect(await talentir.tokenURI(tokenID)).to.equal("ipfs://" + uri);
+    expect(await talentir.tokenOfOwnerByIndex(user1Address, 0)).to.equal(tokenID);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
