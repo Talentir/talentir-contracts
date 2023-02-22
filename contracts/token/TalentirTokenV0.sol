975a035db4cbbe0dadd93f4c83ca886e608160da
talentir-cp <cs@talentir.com>
2023-02-22 12:28:48 +0100

    Implement remaining reviewer suggestions

--- contracts/token/TalentirTokenV0.sol
+++ contracts/token/TalentirTokenV0.sol
@@ -17,6 +17,8 @@ contract TalentirTokenV0 is ERC1155(""), ERC2981, Ownable, Pausable {
 
     // - EVENTS
     event MarketplaceApproved(address marketplaceAddress, bool approved);
+    event RoyaltyPercentageChanged(uint256 percent);
+    event TalentChanged(address from, address to, uint256 tokenID);
 
     // - ADMIN FUNCTIONS
     // At the beginning, these are centralized with Talentir but should be handled by the
@@ -39,8 +41,9 @@ contract TalentirTokenV0 is ERC1155(""), ERC2981, Ownable, Pausable {
      * @notice Changing the royalty percentage of every sale. 1% = 1_000
      */
     function setRoyalty(uint256 percent) public onlyOwner {
-        require(percent <= 100_000, "Must be <= 100%");
-        _setRoyaltyPercent(percent);
+        require(percent <= 10_000, "Must be <= 10%");
+        _royaltyPercent = percent;
+        emit RoyaltyPercentageChanged(percent);
     }
 
     function setMinterRole(address minterAddress) public onlyOwner {
@@ -51,7 +54,7 @@ contract TalentirTokenV0 is ERC1155(""), ERC2981, Ownable, Pausable {
      * @notice Approve a new Marketplace Contract so users need less gas when selling and buying NFTs
      * on the Talentir contract.
      */
-    function setNftMarketplaceApproval(address marketplace, bool approval) public onlyOwner {
+    function approveNftMarketplace(address marketplace, bool approval) public onlyOwner {
         approvedMarketplaces[marketplace] = approval;
         emit MarketplaceApproved(marketplace, approval);
     }
@@ -80,18 +83,23 @@ contract TalentirTokenV0 is ERC1155(""), ERC2981, Ownable, Pausable {
         _mint(to, tokenId, TOKEN_FRACTIONS, "");
     }
 
-    /**
-     * @notice Burn a token. This is only possible for the owner of the token.
-     */
-    function burn(address account, uint256 tokenID, uint256 value) public virtual onlyOwner {
-        _burn(account, tokenID, value);
-    }
-
     function uri(uint256 tokenId) public view override returns (string memory) {
         return string(abi.encodePacked("ipfs://", _tokenCIDs[tokenId]));
     }
 
     // - PUBLIC FUNCTIONS
+
+    function updateTalent(uint256 tokenId, address talent) public {
+        address currentReceiver = _talents[tokenId];
+        require(currentReceiver == msg.sender, "Royalty receiver must update");
+        _setTalent(tokenId, talent);
+    }
+
+    function _setTalent(uint256 tokenID, address talent) internal {
+        address from = _talents[tokenID];
+        _talents[tokenID] = talent;
+        emit TalentChanged(from, talent, tokenID);
+    }
 
     /**
      * @notice A pure function to calculate the tokenID from a given unique contentID. A contentID
